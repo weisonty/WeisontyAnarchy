33c5c7eab3b408415e0a72622fa32e796ca44c8d
weisonty <wesontygg@gmail.com>
2026-06-10 11:20:48 +0500

    Add plugins/AntiCheatLite/src/AntiCheatLite/checks/SpeedCheck.php

---
+++ plugins/AntiCheatLite/src/AntiCheatLite/checks/SpeedCheck.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace AntiCheatLite\checks;
+
+use AntiCheatLite\Main;
+use pocketmine\player\Player;
+
+class SpeedCheck {
+    private Main $plugin;
+    public function __construct(Main $plugin) { $this->plugin = $plugin; }
+    public function flag(Player $player, string $detail): void {
+        $this->plugin->addViolation($player->getName(), "SPEED", $detail);
+    }
+}
